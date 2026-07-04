16240b88bea68ef346f3c6d6922b53c8f9ec6b42
NagaSudheer <96164492+NeravatiNagaSudheer@users.noreply.github.com>
2026-07-04 21:47:17 +0530

    Update UserController.java

--- 21-spring-boot-logging/Code/UserController.java
+++ 21-spring-boot-logging/Code/UserController.java
@@ -1,1 +1,26 @@
+package com.example.logging.controller;
+
+import com.example.logging.service.LoggingExample;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final LoggingExample loggingExample;
+
+    public UserController(LoggingExample loggingExample) {
+        this.loggingExample = loggingExample;
+    }
+
+    @GetMapping("/users")
+    public String getUsers() {
+
+        loggingExample.fetchUsers();
+
+        return "Users Retrieved Successfully";
+
+    }
+
+}
 
