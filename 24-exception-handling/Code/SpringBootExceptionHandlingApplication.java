2469a715853830e65cc09555afd39a5d37858027
NagaSudheer <96164492+NeravatiNagaSudheer@users.noreply.github.com>
2026-07-08 00:15:02 +0530

    Update SpringBootExceptionHandlingApplication.java

--- 24-exception-handling/Code/SpringBootExceptionHandlingApplication.java
+++ 24-exception-handling/Code/SpringBootExceptionHandlingApplication.java
@@ -1,1 +1,18 @@
 
+package com.example.exceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExceptionHandlingApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                SpringBootExceptionHandlingApplication.class,
+                args);
+
+    }
+
+}
