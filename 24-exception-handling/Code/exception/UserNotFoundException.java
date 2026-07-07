40b1b25d84fa3b0127f1644558b794b3e7e119e5
NagaSudheer <96164492+NeravatiNagaSudheer@users.noreply.github.com>
2026-07-08 00:15:42 +0530

    Update UserNotFoundException.java

--- 24-exception-handling/Code/exception/UserNotFoundException.java
+++ 24-exception-handling/Code/exception/UserNotFoundException.java
@@ -1,1 +1,12 @@
 
+package com.example.exceptionhandling.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+
+        super(message);
+
+    }
+
+}
