79dea58d5784d4d102a290f65e12747a1d22fc50
NagaSudheer <96164492+NeravatiNagaSudheer@users.noreply.github.com>
2026-07-10 00:38:34 +0530

    Implement UserController for user retrieval

--- 25-global-exception-handling/Code/controller/UserController.java
+++ 25-global-exception-handling/Code/controller/UserController.java
@@ -1,1 +1,24 @@
+package com.example.exceptionhandling.controller;
 
+import com.example.exceptionhandling.model.User;
+import com.example.exceptionhandling.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+
+        return userService.getUserById(id);
+
+    }
+
+}
