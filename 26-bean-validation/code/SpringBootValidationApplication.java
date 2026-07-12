cf1af3016828c88e4bcf50104eea4f50764470d9
NagaSudheer <96164492+NeravatiNagaSudheer@users.noreply.github.com>
2026-07-12 22:30:43 +0530

    Add Spring Boot application entry point

--- 26-bean-validation/code/SpringBootValidationApplication.java
+++ 26-bean-validation/code/SpringBootValidationApplication.java
@@ -1,1 +1,18 @@
+package com.example.validation;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootValidationApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                SpringBootValidationApplication.class,
+                args
+        );
+
+    }
+
+}
