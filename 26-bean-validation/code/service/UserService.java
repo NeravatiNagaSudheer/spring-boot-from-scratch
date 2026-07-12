35ce6ce4d3837adc9127a10725cf2d13b26fe689
NagaSudheer <96164492+NeravatiNagaSudheer@users.noreply.github.com>
2026-07-12 22:26:51 +0530

    Update UserService.java

--- 26-bean-validation/code/service/UserService.java
+++ 26-bean-validation/code/service/UserService.java
@@ -1,1 +1,20 @@
+package com.example.validation.service;
 
+import com.example.validation.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public void saveUser(User user) {
+
+        // Simulate saving the user
+        System.out.println("Saving User...");
+
+        System.out.println("Name  : " + user.getName());
+        System.out.println("Email : " + user.getEmail());
+        System.out.println("Age   : " + user.getAge());
+
+    }
+
+}
