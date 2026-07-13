713f90da8bbab6d18b65c475f7ccc9f5bb7275bf
NagaSudheer <96164492+NeravatiNagaSudheer@users.noreply.github.com>
2026-07-13 22:59:03 +0530

    Implement UserService with createUser method
    Added UserService class for user management with a createUser method.

--- 27-common-validation-annotations/code/service/UserService.java
+++ 27-common-validation-annotations/code/service/UserService.java
@@ -1,1 +1,20 @@
+package com.example.validation.service;
 
+import com.example.validation.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public void createUser(User user) {
+
+        System.out.println("Saving user...");
+
+        System.out.println("Name    : " + user.getName());
+        System.out.println("Email   : " + user.getEmail());
+        System.out.println("Age     : " + user.getAge());
+        System.out.println("Mobile  : " + user.getMobile());
+
+    }
+
+}
