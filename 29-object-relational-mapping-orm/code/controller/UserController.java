4c0105d294135ec4e944248ac0b821f993f58da3
NagaSudheer <96164492+NeravatiNagaSudheer@users.noreply.github.com>
2026-07-17 01:28:07 +0530

    Implement UserController for user management

--- 29-object-relational-mapping-orm/code/controller/UserController.java
+++ 29-object-relational-mapping-orm/code/controller/UserController.java
@@ -1,1 +1,29 @@
+package com.example.orm.controller;
 
+import com.example.orm.model.User;
+import com.example.orm.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createUser(
+            @RequestBody User user) {
+
+        userService.saveUser(user);
+
+        return ResponseEntity.ok(
+                "User saved successfully (ORM Example).");
+
+    }
+
+}
